b1c8a20f7084f132813f233408e234cd7e3d0b03
Justin Feldstein <92735599+justinfeldstein@users.noreply.github.com>
2022-04-01 10:40:16 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+
+
+    // Function to
+    const playGame = () => {
+        const rockBtn = document.querySelector('.rock');
+        const paperBtn = document.querySelector('.paper');
+        const scissorBtn = document.querySelector('.scissor');
