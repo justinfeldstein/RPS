9db2302aeb801fbc025379468a46b0571aefffa7
Justin Feldstein <92735599+justinfeldstein@users.noreply.github.com>
2022-04-07 11:52:53 -0400

    Add files via upload

---
+++ javascipt.js
@@ -0,0 +1,76 @@
+let playerScore = 0;
+ let computerScore = 0;
+ let playerChoice = "";
+ let computerChoice = "";
+ let pCount = document.getElementById('p-count');
+ let cCount = document.getElementById('c-count');
+
+function computerSelect() {
+ let choice = Math.floor(Math.random() * 3);
+ switch (choice) {
+   case 0:
+     computerChoice = "paper";
+     break;
+   case 1:
+     computerChoice = "rock";
+     break;
+   case 2:
+     computerChoice = "scissors";
+     break;
+ }
+}
+
+function Dwayne() {
+ computerSelect();
+ switch (computerChoice) {
+   case "paper":
+     computerScore++;
+     break;
+   case "rock":
+     break;
+   case "scissors":
+     playerScore++;
+     break;
+ }
+ pCount.innerHTML = playerScore;
+ cCount.innerHTML = computerScore;
+}
+
+function Konan() {
+ computerSelect();
+ switch (computerChoice) {
+   case "scissors":
+     computerScore++;
+     break;
+   case "paper":
+     break;
+   case "rock":
+     playerScore++;
+     break;
+ }
+ pCount.innerHTML = playerScore;
+ cCount.innerHTML = computerScore;
+}
+
+function Scizor() {
+ computerSelect();
+ switch (computerChoice) {
+   case "rock":
+     computerScore++;
+     break;
+   case "scissors":
+     break;
+   case "paper":
+     playerScore++;
+     break;
+ }
+ pCount.innerHTML = playerScore;
+ cCount.innerHTML = computerScore;
+}
+
+function reset() {
+ computerScore = 0;
+ playerScore = 0;
+ pCount.innerHTML = playerScore;
+ cCount.innerHTML = computerScore;
+}
